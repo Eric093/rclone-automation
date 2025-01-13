a2287430126f9fe0a063acd4fc3d7faccf98dfe5
Eric093 <eric.blot1@gmail.com>
2025-01-13 16:42:11 +0100

    Ajout de messages d'état pour vérifier si rclone est installé

--- rclone-1.py
+++ rclone-1.py
@@ -15,7 +15,12 @@ def is_installed():
 
 print(is_installed())
 
+if is_installed():
+    print("rclone is installed")
 
+if not is_installed():
+    print("rclone is not installed")
+    
 #print(rclone.version())  # Get the version of rclone
 
 #print(rclone.get_remotes())  # Get a list of available remotes
